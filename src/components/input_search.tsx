c697930ab6bc01239ba2f021c8d67364a08616ed
denisfl1 <denisfl1@hotmail.com>
2024-06-22 10:47:52 -0300

    firstUpdate

--- src/components/input_search.tsx
+++ src/components/input_search.tsx
@@ -1,22 +1,22 @@
 import uncheck from "../images/unchecked.png"
 import check from "../images/checked.png"
 import down_unclicked from "../images/down-gray.png"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
-interface object_Data{
-    id:string,
-    title:string,
-    isDone:boolean,
-    map:(data:any)=>object_Data
-}
 
 const InputSearch = ()=>{
 
-    const[search,setSeach] = useState()
-    const [data,setData] = useState<any>(
+    interface object_Data{
+        id:string,
+        title:string,
+        isDone:boolean,
+        map:(data:any)=>void
+    }
+    
 
-        [
+    const[search,setSeach] = useState()
+    const [data,setData] = useState<any>([
             {
               id: "flrGI",
               title: "Lavar os pratos",
@@ -32,22 +32,17 @@ const InputSearch = ()=>{
               title: "Comprar pão",
               isDone: false
             }
-          ]
+          ])
 
-    )
+    useEffect(()=>{
+
+    },[])
 
     const HandleCheck=(id:string,parameter:boolean)=>{
-        console.log(data)
-        if(parameter == true)return setData((it:object_Data)=>{
-            it.map((val:object_Data)=>{
-               return val.id == id && {...val,isDone:true}
-            })
-        })
-        return setData((it:object_Data)=>{
-            it.map((val:object_Data)=>{
-               return val.id == id && {...val,isDone:false}
-            })
-        })
+        
+        alert(parameter)
+        
+        
 
     }
 
@@ -56,15 +51,15 @@ return(
     <div className="inputContainer_Master">
         <div className="inputContainer"><img className="down_unclicked" src={down_unclicked}></img><input placeholder="What needs to be done?"></input></div>
         <div className="container_search_select">
-           {data[0] && <ul className="search_li">
+           <ul className="search_li">
                 {data.map((items:object_Data)=>{
                     return(
                         <>
-                         <li><img onClick={()=>HandleCheck(items.id,items.isDone)} src={items.isDone ? check : uncheck}></img>{items.title}</li>                   
+                         <li className={`${items.id}`}><img onClick={()=>HandleCheck(items.id,!items.isDone)} src={items.isDone ? check : uncheck}></img>{items.title}</li>                   
                         </>
                     )
                 })}
-            </ul>}
+            </ul>
             <div className="container_status">   <span style={{marginLeft:'-20px'}}>{`${1} item left`}</span>  <div className="container_select_option"> <div className="select_span"><span>All</span></div>  <div className="select_span selected"><span>Active</span></div>   <div className="select_span"><span>Completed</span></div></div> <span className="clear_completed">Clear Completed</span></div>
         </div>
     </div>
